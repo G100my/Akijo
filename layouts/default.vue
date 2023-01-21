eed8130bb5b4dcd87acb62292db28f27cc92a6e4
G100 <g100my.lan229@gmail.com>
2023-01-22 03:38:02 +0800

    feat: layout - default

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+const { isMounted } = useJudgeMounted()
+</script>
+
+<template>
+  <div class="drawer drawer-end">
+    <div class="drawer-content fixed top-0 flex flex-col">
+      <!-- Page content here -->
+      <slot />
+    </div>
+
+    <Navbar v-if="isMounted" />
+  </div>
+</template>
+
+<style scoped></style>
