430452c12664950fa9b0d173b50400d0790301d8
G100 <g100my.lan229@gmail.com>
2023-01-22 00:07:05 +0800

    core: add Google font (icon) CDN

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,16 @@ import zh from './locales/zh.json'
 export default defineNuxtConfig({
   ssr: false,
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/i18n', 'nuxt-typed-router'],
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@40,600,0,0',
+        },
+      ],
+    },
+  },
   i18n: {
     vueI18n: {
       legacy: false,
