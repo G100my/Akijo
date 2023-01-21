398f87b255fa4bcab93ac6b61591caa8cf7da0bb
G100 <g100my.lan229@gmail.com>
2023-01-21 21:09:09 +0800

    core: init @nuxtjs/i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
+import en from './locales/en.json'
+import zh from './locales/zh.json'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/i18n'],
+  i18n: {
+    vueI18n: {
+      legacy: false,
+      locale: 'zh',
+      messages: { en, zh },
+    },
+  },
 })
