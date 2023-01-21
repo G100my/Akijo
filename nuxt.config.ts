f1369406f275fc55685369ab1dff5bbfca8aa885
G100 <g100my.lan229@gmail.com>
2023-01-21 21:22:28 +0800

    feat: nuxt.config - ssr: false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ import en from './locales/en.json'
 import zh from './locales/zh.json'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/i18n'],
   i18n: {
     vueI18n: {
