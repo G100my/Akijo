5c8c1a00e7fbc3cdd57a98b90443da3daafbbb4b
G100 <g100my.lan229@gmail.com>
2023-01-22 03:41:09 +0800

    feat(page): init index, About, ServiceMenu

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<script>
+// tmp
+</script>
+<template>
+  <section id="index">
+    <header class="hero min-h-screen" style="background-image: url(https://fakeimg.pl/300?text=hero_image)">
+      <div class="hero-overlay bg-opacity-60" />
+
+      <div class="hero-content text-center text-neutral-content">
+        <div class="max-w-md">
+          <h1 class="mb-5 text-5xl font-bold">Akijo title</h1>
+          <p class="mb-5">Akijō is a carrier like amoeba We gather mtfk talented badass</p>
+          <button class="btn-primary btn">Get Started</button>
+        </div>
+      </div>
+    </header>
+    <main></main>
+    <footer></footer>
+  </section>
+</template>
