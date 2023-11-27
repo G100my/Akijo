7f3f5603410339c4c20515d07172e63732253e15
G100 <g100mylan229@gmail.com>
2023-11-27 16:30:55 +0800

    update style

--- components/NotificationBlock.vue
+++ components/NotificationBlock.vue
@@ -7,6 +7,19 @@ function handleActive() {
     status.value = Notification.permission
   })
 }
+
+const LinkElements = () =>
+  h('p', [
+    h('span', '操作說明連結：'),
+    h('span', { class: 'whitespace-nowrap' }, [
+      h(
+        'a',
+        { class: 'text-sky-400', target: '_blank', href: 'https://support.google.com/chrome/answer/3220216?hl=zh-Hant&co=GENIE.Platform%3DAndroid' },
+        'Google chrome',
+      ),
+      h('a', { class: 'text-sky-400 ml-5', target: '_blank', href: 'https://support.apple.com/zh-tw/guide/safari/sfri40734/mac' }, 'Safari'),
+    ]),
+  ])
 </script>
 <template>
   <div class="text-center space-y-4">
@@ -19,13 +32,7 @@ function handleActive() {
       <div class="flex flex-col mt-4">
         <p>你隨時可以把Akijo的瀏覽器通知關閉</p>
         <div class="text-sm mx-auto">
-          <p class="space-x-5">
-            <span>操作說明連結：</span>
-            <a class="text-sky-400" target="_blank" href="https://support.google.com/chrome/answer/3220216?hl=zh-Hant&co=GENIE.Platform%3DAndroid"
-              >Google chrome</a
-            >
-            <a class="text-sky-400" target="_blank" href="https://support.apple.com/zh-tw/guide/safari/sfri40734/mac">Safari</a>
-          </p>
+          <LinkElements />
         </div>
       </div>
       <button type="button" @click="handleActive" class="heartbeat bg-sky-400 py-2 px-4 rounded">
@@ -36,23 +43,13 @@ function handleActive() {
     <template v-else-if="status === 'granted'">
       <p>如果萩条生活的通知太過打擾你的話，<br />你可以參考以下連結操作。</p>
       <div class="text-sm mx-auto">
-        <p class="space-x-5">
-          <span>操作說明連結：</span>
-          <a class="text-sky-400" target="_blank" href="https://support.google.com/chrome/answer/3220216?hl=zh-Hant&co=GENIE.Platform%3DAndroid"
-            >Google chrome</a
-          >
-          <a class="text-sky-400" target="_blank" href="https://support.apple.com/zh-tw/guide/safari/sfri40734/mac">Safari</a>
-        </p>
+        <LinkElements />
       </div>
     </template>
     <template v-else-if="status === 'denied'">
       <p>咦？你把我們的活動通知禁止了？<br />我們真的不會太常打擾你，<br />只會在有趣的萩条生活<br />將要發生的時候通知你，<br />一起來過萩条生活</p>
       <p class="text-base">現在瀏覽器把使用者保護的很好，<br />我們無法直接幫你再次開啟通知，<br />你可以參考以下連結重新設定。</p>
-      <p class="space-x-5">
-        <span>操作說明連結：</span>
-        <a class="text-sky-400" target="_blank" href="https://support.google.com/chrome/answer/3220216?hl=zh-Hant&co=GENIE.Platform%3DAndroid">Google chrome</a>
-        <a class="text-sky-400" target="_blank" href="https://support.apple.com/zh-tw/guide/safari/sfri40734/mac">Safari</a>
-      </p>
+      <LinkElements />
     </template>
   </div>
 </template>
